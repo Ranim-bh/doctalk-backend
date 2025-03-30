06f159ce11d3db12f8f12d0fca235eecadb2eafa
Ranim-bh <ranimbenhmida2912@gmail.com>
2025-03-30 01:42:16 +0100

    Delete dbConfig.js

--- dbConfig.js
+++
@@ -1,7 +0,0 @@
-const dbConfig = {
-    user: "SYSTEM",  // Remplace avec ton utilisateur Oracle
-    password: "123456789",  // Remplace avec ton mot de passe Oracle
-    connectString: "localhost:1521/XE"  // Connexion à Oracle XE
-};
-
-module.exports = dbConfig;
